4f2c7e53f2ffaca173c5c83f12f26778fe925635
Jay Vigilla <jayrvigilla@gmail.com>
2024-04-09 16:25:16 -0700

    FeatureIcons uses MUI Icons instead of FontAwesome

--- src/components/FeatureIcon/FeatureIcon.tsx
+++ src/components/FeatureIcon/FeatureIcon.tsx
@@ -1,35 +1,33 @@
 /** FeatureIcon documentation
  * displays the appropriate FontAwesome svg given the prop
  */
-import React, { useEffect, useState } from "react";
+import React, { ReactNode, useEffect, useState } from "react";
 import "./style.css";
-import {FontAwesomeIcon}  from "@fortawesome/react-fontawesome";
+// import {FontAwesomeIcon}  from "@fortawesome/react-fontawesome";
 
-import { faBed, faBathtub, faSwimmingPool, faFan, faCar } from "@fortawesome/free-solid-svg-icons";
-
+// import { faBed, faBathtub, faSwimmingPool, faFan, faCar, faHouse } from "@fortawesome/free-solid-svg-icons";
+import { BedOutlined, BathtubOutlined, PoolOutlined, GarageOutlined } from "@mui/icons-material";
 
 export interface FeatureIconProps {
   "data-test-id"?: string;
-  variant: "bed" | "bath" | "pool";
+  variant: "bed" | "bath" | "pool" | "garage";
   label?: string;
 }
 
-export const FeatureIcon = ({variant,label}: FeatureIconProps) => {
+export const FeatureIcon = ({ variant, label }: FeatureIconProps) => {
+  const iconProps = {
+    fontSize: "large"
+  }
   const variantToIcon = {
-    bed: faBed,
-    bath: faBathtub,
-    pool: faSwimmingPool
+    bed: <BedOutlined fontSize="large"   />,
+    bath: <BathtubOutlined fontSize="large" />,
+    pool: <PoolOutlined fontSize="large" />,
+    garage: <GarageOutlined fontSize="large" />,
   }
 
   return (
     <span className="feature-icon-container">
-  <span className="feature-icon">
-    <FontAwesomeIcon
-      size="lg"
-      icon={variantToIcon[variant]}
-      color="blue"
-      />
-    </span>
+      <span className="feature-icon">{variantToIcon[variant]}</span>
       {label && <span className="label">{label}</span>}
     </span>
   )
