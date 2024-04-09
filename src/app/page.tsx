9b615416c96a1fd0ce89bb844d814c472d879c72
Jay Vigilla <jayrvigilla@gmail.com>
2024-04-08 21:33:38 -0700

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+// import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.home}>
+      <h1>Boiler Plate Empty</h1>
+    </div>
+  );
+}
