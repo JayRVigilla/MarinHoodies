c5a66f1f09e6faf23e703edf90faece6c5da2d8c
Jay Vigilla <jayrvigilla@gmail.com>
2024-04-30 18:00:13 -0700

    clean

--- src/lib/marinFoodInspection.ts
+++ src/lib/marinFoodInspection.ts
@@ -15,33 +15,35 @@ export const getFoodInspections = async (queries: tFoodInspectionQueries) => {
      *
      */
     const url = `${MARIN_FOOD_INSPECTION_BASE_URL}?$where=${queries}`
-    const data: Promise<iFoodInspectionAPIInspection[]> = fetch(url)
+    const data = fetch(url)
       .then(response => response.json())
-      .then(result => { return result})
+      .then(res => {
+        const result = []
+        
+        for (let inspection of res) {
+          result.push({
+            business_name: inspection.business_name,
+            formatted_address: inspection.formatted_address,
+            latitude: inspection.businessaddress.latitude,
+            longitude: inspection.businessaddress.longitude,
+            inspection_date: inspection.inspection_date,
+            inspection_type: inspection.inspection_type,
+            inspector: inspection.inspector,
+            inspector_comments: inspection.inspector_comments,
+            inspector_freqeuncy: inspection.inspection_frequency,
+            inspection_description: inspection.inspection_description,
+            is_major_violation: inspection.is_major_violation,
+            correct_by_date: inspection.correct_by_date,
+            corrected_on_site: inspection.corrected_on_site,
+            violation_description: inspection.violation_description,
+            placard: inspection.placard,
+          })
+        }
+
+        return result
+      })
     console.log("getFoodInspections", data)
 
-    const result = []
-    // TODO: do this in .then() on line 20 before returning result
-    for (let inspection of data) {
-      // TODO: ouput looks like iCrimeLocationMarker[]
-      result.push({
-        business_name: inspection.business_name,
-        formatted_address: inspection.formatted_address,
-        latitude: inspection.businessaddress.latitude,
-        longitude: inspection.businessaddress.longitude,
-        inspection_date: inspection.inspection_date,
-        inspection_type: inspection.inspection_type,
-        inspector: inspection.inspector,
-        inspector_comments: inspection.inspector_comments,
-        inspector_freqeuncy: inspection.inspection_frequency,
-        inspection_description: inspection.inspection_description,
-        is_major_violation: inspection.is_major_violation,
-        correct_by_date: inspection.correct_by_date,
-        corrected_on_site: inspection.corrected_on_site,
-        violation_description: inspection.violation_description,
-        placard: inspection.placard,
-      })
-    }
 
     return data
   } catch (error) {
