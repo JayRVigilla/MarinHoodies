08f0cb420a6087625e96daf81d399463d432a691
Jay Vigilla <jayrvigilla@gmail.com>
2024-06-13 10:19:42 -0700

    clean

---
+++ src/components/PropertySearchForm/PropertySearchForm.tsx
@@ -0,0 +1,91 @@
+/** PropertySearchForm documentation
+ */
+"use client"
+import {useCallback, useState, Dispatch} from 'react'
+import "./styles.css";
+import { TextInput } from '../TextInput';
+import { Button } from '../Button';
+import { getLongLatFromAddress } from '@/src/lib/positionstack';
+import { tCoordsObject } from '@/src/constants';
+import { DateRangeSelector } from '../DateRangeSelector/DateRangeSelector';
+import { iCrimeLocationMarker, iFoodInspectionMarker } from '../Map/Marker/types';
+import { getFoodInspections } from '@/src/lib/marinFoodInspection';
+import { getCrimes } from '@/src/lib/marinCrime';
+
+export interface iPropertySearchFormProps {
+  "data-test-id"?: string;
+  setLocationLatLong: Dispatch<tCoordsObject>;
+  setFoodInspections: Dispatch<iFoodInspectionMarker[]>;
+  setCrimes: Dispatch<iCrimeLocationMarker[]>;
+}
+
+export const PropertySearchForm = ({setLocationLatLong, setFoodInspections, setCrimes}: iPropertySearchFormProps) => {
+  // * state
+  const [address, setAddress] = useState("");
+  const [city, setCity] = useState("");
+  const [state, setState] = useState("");
+  const [whereFilter, setWhereFilter] = useState<string | undefined>(undefined);
+  // [MinISOString, MaxISOString]
+  const [dateRange, setDateRange] = useState<[string, string]>(["", ""]);
+
+  const clearForm = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault()
+    if(!address && !city && !state)return
+    setAddress("")
+    setCity("")
+    setState("")
+  }
+
+  const submitSearch = useCallback(async (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault()
+    if(!address && !city && !state) return
+    const locationData = await getLongLatFromAddress({ address, city, state })
+    if (locationData) {
+      setLocationLatLong(locationData)
+
+      const inspectionsData = await getFoodInspections({
+        dateRange: dateRange,
+        focalLatLong: [locationData.lat, locationData.lon]
+      })
+      if(inspectionsData) setFoodInspections(inspectionsData)
+
+      const crimeData = await getCrimes({
+        dateRange: dateRange,
+        focalLatLong: [locationData.lat, locationData.lon]
+      })
+      setCrimes(crimeData!)
+      }
+  }, [address, city, state, dateRange])
+
+  return (
+    <form className='property-search-form'>
+
+
+      <TextInput value={address} placeholder='' setValue={setAddress} label="address" />
+    <span className='input-group'>
+      <TextInput value={city } placeholder='' setValue={setCity } label="city" />
+      <TextInput value={state } placeholder='' setValue={setState } label="state" />
+    </span>
+
+      <DateRangeSelector
+        whereFilter={whereFilter}
+        setWhereFilter={setWhereFilter}
+        dateRange={dateRange}
+        setDateRange={setDateRange}
+      />
+
+
+    <div className='action-buttons'>
+
+      <Button
+        label='Search'
+        onClick={(event)=>submitSearch(event)}
+      />
+      <Button
+        label='Clear'
+        onClick={(event)=>clearForm(event)}
+      />
+    </div>
+  </form>
+  );
+};
---
+++ src/components/PropertySearchForm/index.tsx
@@ -0,0 +1,3 @@
+import { PropertySearchForm } from "./PropertySearchForm"
+import { TextInput } from "../TextInput/TextInput"
+export {PropertySearchForm, TextInput}
