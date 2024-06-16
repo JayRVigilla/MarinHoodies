7f5f0598b706e29fd043d43c50dc7ce2da2d877e
Jay Vigilla <jayrvigilla@gmail.com>
2024-06-15 22:21:12 -0700

    clean

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,20 @@
 "use client";
-import { Landscape } from "@mui/icons-material";
 import "./page.css";
-import { Map } from "../components/Map";
-import { useState } from "react";
-import {
-  iCrimeLocationMarker,
-  iFoodInspectionMarker,
-} from "../components/Map/Marker/types";
 import { PropertySearchForm } from "../components/PropertySearchForm";
-import { tCoordsObject } from "../constants";
 import { Logo } from "../components/Logo";
 
 export default function Home() {
-  // const [crimes, setCrimes] = useState<iCrimeLocationMarker[]>([])
-  // const [foodInspections, setFoodInspections] = useState<iFoodInspectionMarker[]>([])
-  // const [locationLatLong, setLocationLatLong] = useState<tCoordsObject>()
 
   return (
     <div className="home root">
       <Logo />
       <div className="into-content">
         <p>Welcome to MarinHoodies!</p>
-        <p>We are sharing information about Marin County to make your search for a home or rental bsed on real data from county services. </p>
+        <p>We are sharing information about Marin County to make your search for a home or rental based on real data from county services. </p>
         <p>Start by entering the address below to see a map and relevant permit data for the address</p>
       </div>
 
       <PropertySearchForm />
-      
-      {/* <div className="map-container">
-
-        {locationLatLong && <Map
-          crimes={crimes}
-          foodInspections={foodInspections}
-          locationLatLong={locationLatLong}
-        />}
-
-      </div> */}
     </div>
   );
 }
--- src/components/PropertySearchForm/PropertySearchForm.tsx
+++ src/components/PropertySearchForm/PropertySearchForm.tsx
@@ -1,92 +1,67 @@
 /** PropertySearchForm documentation
  */
-"use client"
-import {useCallback, useState, Dispatch} from 'react'
+"use client";
+import { useCallback, useState } from "react";
 import "./styles.css";
-import { TextInput } from '../TextInput';
-import { Button } from '../Button';
-import { getLongLatFromAddress } from '@/src/lib/positionstack';
-import { tCoordsObject } from '@/src/constants';
-import { DateRangeSelector } from '../DateRangeSelector/DateRangeSelector';
-import { iCrimeLocationMarker, iFoodInspectionMarker } from '../Map/Marker/types';
-import { getFoodInspections } from '@/src/lib/marinFoodInspection';
-import { getCrimes } from '@/src/lib/marinCrime';
+import { TextInput } from "../TextInput";
+import { Button } from "../Button";
+import { getLongLatFromAddress } from "@/src/lib/positionstack";
 import { useRouter } from "next/navigation";
 
 export interface iPropertySearchFormProps {
   "data-test-id"?: string;
-  // setLocationLatLong: Dispatch<tCoordsObject>;
-  // setFoodInspections: Dispatch<iFoodInspectionMarker[]>;
-  // setCrimes: Dispatch<iCrimeLocationMarker[]>;
 }
 
-export const PropertySearchForm = ({ }: iPropertySearchFormProps) => {
-  const router = useRouter()
+export const PropertySearchForm = ({}: iPropertySearchFormProps) => {
+  const router = useRouter();
 
   // * state
   const [address, setAddress] = useState("");
   const [city, setCity] = useState("");
-  // const [state, setState] = useState("");
-  // const [whereFilter, setWhereFilter] = useState<string | undefined>(undefined);
-  // [MinISOString, MaxISOString]
-  // const [dateRange, setDateRange] = useState<[string, string]>(["", ""]);
 
   const clearForm = (event: React.MouseEvent<HTMLButtonElement>) => {
-    event.preventDefault()
-    // if(!address && !city && !state)return
-    if(!address && !city )return
-    setAddress("")
-    setCity("")
-    // setState("")
-  }
+    event.preventDefault();
+    if (!address && !city) return;
+    setAddress("");
+    setCity("");
+  };
 
-  const submitSearch = useCallback(async (event: React.MouseEvent<HTMLButtonElement>) => {
-    event.preventDefault()
-    // if(!address && !city && !state) return
-    if(!address && !city) return
-    const locationData = await getLongLatFromAddress({ address, city, state: "CA" })
-    if (locationData) {
-      const params = new URLSearchParams({
-        ...locationData,
+  const submitSearch = useCallback(
+    async (event: React.MouseEvent<HTMLButtonElement>) => {
+      event.preventDefault();
+      if (!address && !city) return;
+      const locationData = await getLongLatFromAddress({
         address,
         city,
         state: "CA",
       });
-      router.push(`/search?${params.toString()}`);
+      if (locationData) {
+        const params = new URLSearchParams({
+          ...locationData,
+          address,
+          city,
+          state: "CA",
+        });
+        router.push(`/search?${params.toString()}`);
       }
-
-  }, [address, city])
+    },
+    [address, city]
+  );
 
   return (
-    <form className='property-search-form'>
-
-
-      <TextInput value={address} placeholder='' setValue={setAddress} label="address" />
-
-      <TextInput value={city } placeholder='' setValue={setCity } label="city" />
-      {/* <TextInput value={state } placeholder='' setValue={setState } label="state" /> */}
-      {/* <span className='input-group'>
-    </span> */}
-
-      {/* <DateRangeSelector
-        whereFilter={whereFilter}
-        setWhereFilter={setWhereFilter}
-        dateRange={dateRange}
-        setDateRange={setDateRange}
-      /> */}
-
-
-    <div className='action-buttons'>
-
-      <Button
-        label='Search'
-        onClick={(event)=>submitSearch(event)}
+    <form className="property-search-form">
+      <TextInput
+        value={address}
+        placeholder=""
+        setValue={setAddress}
+        label="address"
       />
-      <Button
-        label='Clear'
-        onClick={(event)=>clearForm(event)}
-      />
-    </div>
-  </form>
+      <TextInput value={city} placeholder="" setValue={setCity} label="city" />
+
+      <div className="action-buttons">
+        <Button label="Search" onClick={(event) => submitSearch(event)} />
+        <Button label="Clear" onClick={(event) => clearForm(event)} />
+      </div>
+    </form>
   );
 };
